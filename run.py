a100b16f23d8984275a926a281813396a700ffd3
jd-rub <justin.dettmer@rub.de>
2025-09-19 18:34:08 +0200

    fix missing import

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from pytorch_lightning.cli import LightningCLI
 from training.passt_adv_train import PasstAdv
+from training.passt_vs_random import PasstRandom
 from training.cnn14_adv_train import CNN14Adv
 from training.cnn14_vs_random import CNN14Random
 from data.esc50 import ESC50DataModule
